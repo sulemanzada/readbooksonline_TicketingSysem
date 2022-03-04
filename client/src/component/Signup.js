14b06894c0551ca8d21a5015a55ac82e855cde8d
charles3019 <charles3019@my.shu.ac.uk>
2022-03-04 07:01:05 +0500

    update8_Improved_SignUpPage

--- client/src/component/Signup.js
+++ client/src/component/Signup.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-
+import { NavLink } from "react-router-dom";
 const Signup = () => {
     const [records, setRecords] = useState([]);
     const [userRegistration, setuserRegistration] = useState({
-        username: "",
+        fname: "",
+        lname: "",
         email: "",
         phone: "",
-        password: ""
+        password: "",
+        cpassword: ""
     });
     const handleInput = (e) => {
         const name = e.target.name;
@@ -23,76 +25,78 @@ const Signup = () => {
         setRecords([...records, newRecord]);
         console.log(records);
 
-        setuserRegistration({ username: "", email: "", phone: "", password: "" });
+        setuserRegistration({ fname: "", lname: "", email: "", phone: "", password: "", cpassword: "" });
     }
     return (
         // React Fragment short form Syntactic sugar
         <>
-            <div class="container register">
-                <div class="row">
-                    <div class="col-md-3 register-left">
+            <div className="container register">
+                <div className="row">
+                    <div className="col-md-3 register-left">
                         <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt="" />
                         <h3>Welcome</h3>
-                        <input type="submit" name="" value="Login" /><br />
-                    </div>
-                    <div class="col-md-9 register-right">
-                        <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
 
-                        </ul>
-                        <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                <h3 class="register-heading">Apply as a Employee</h3>
-                                <div class="row register-form">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="First Name *" value="" />
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="Last Name *" value="" />
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control" placeholder="Password *" value="" />
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control" placeholder="Confirm Password *" value="" />
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="maxl">
-                                                <label class="radio inline">
-                                                    <input type="radio" name="gender" value="male" checked />
-                                                    <span> Male </span>
-                                                </label>
-                                                <label class="radio inline">
-                                                    <input type="radio" name="gender" value="female" />
-                                                    <span>Female </span>
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control" placeholder="Your Email *" value="" />
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" minlength="10" maxlength="10" name="txtEmpPhone" class="form-control" placeholder="Your Phone *" value="" />
-                                        </div>
-                                        <div class="form-group">
-                                            <select class="form-control">
-                                                <option class="hidden" selected disabled>Please select your Sequrity Question</option>
-                                                <option>What is your Birthdate?</option>
-                                                <option>What is Your old Phone Number</option>
-                                                <option>What is your Pet Name?</option>
-                                            </select>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="Enter Your Answer *" value="" />
-                                        </div>
-                                        <input type="submit" class="btnRegister" value="Register" />
+                        <NavLink to="/login">
+                            <input type="submit" name="" value="Login" />
+                            <br /></NavLink>
+                        <p>Already Have Account</p>
+
+                    </div>
+                    <div className="col-md-9 register-right">
+
+
+                        <h3 className="register-heading">Register as a Client</h3>
+
+                        <from className="row register-form" action="" onSubmit={handleSubmit}>
+
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <input type="text" autoCapitalize="off" className="form-control" placeholder="First Name *" value={userRegistration.fname}
+                                        onChange={handleInput} name="fname" id="fname" />
+                                </div>
+                                <div className="form-group">
+                                    <input type="text" autoCapitalize="off" className="form-control" placeholder="Last Name *" value={userRegistration.lname}
+                                        onChange={handleInput} name="lname" id="lname" />
+                                </div>
+                                <div className="form-group">
+                                    <input type="password" autoCapitalize="off" className="form-control" placeholder="Password *" value={userRegistration.password}
+                                        onChange={handleInput} name="password" id="password" />
+                                </div>
+
+                                <div className="form-group">
+                                    <div className="maxl">
+                                        <label className="radio inline">
+                                            <input type="radio" name="gender" value="male" checked />
+                                            <span> Male </span>
+                                        </label>
+                                        <label className="radio inline">
+                                            <input type="radio" name="gender" value="female" />
+                                            <span>Female </span>
+                                        </label>
                                     </div>
                                 </div>
                             </div>
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <input type="email" autoCapitalize="off" className="form-control" placeholder="Your Email *" value={userRegistration.email}
+                                        onChange={handleInput} name="email" id="email" />
+                                </div>
 
-                        </div>
+                                <div className="form-group">
+                                    {/* minlength="10" maxlength="10" */}
+                                    <input type="text" className="form-control" placeholder="Your Phone *" value={userRegistration.phone}
+                                        onChange={handleInput} name="txtEmpPhone" id="txtEmpPhone" />
+                                </div>
+
+                                <div className="form-group">
+                                    <input type="password" autoCapitalize="off" className="form-control" placeholder="Confirm Password *" value={userRegistration.cpassword}
+                                        onChange={handleInput} name="cpassword" id="cpassword" />
+                                </div>
+
+
+                                <input type="submit" className="btnRegister" value="Register" />
+                            </div>
+                        </from>
                     </div>
                 </div>
 
